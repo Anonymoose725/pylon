c1711c5efc0ecc5870ae74292d8fe0838ae374c1
Ethan <ethan.gat@mail.mcgill.ca>
2026-09-21 17:56:37 -0400

    cli directory and multi-file support

--- cli.py
+++ cli.py
@@ -1,22 +1,48 @@
 # https://click.palletsprojects.com/en/stable/
 import click
+from pathlib import Path
 from rules.registry import ALL_RULES
+
+def find_python_files(filepath: str) -> list[Path]:
+    p = Path(filepath)
+    # check if path is valid
+    if not p.exists():
+        click.echo(f"{filepath} is not a valid directory.")
+        return []
+
+    if p.is_file() and p.suffix == ".py":
+        return [p]  # singleton file to append
+    if p.is_file():
+        return []   # not a python file
+
+    return list(p.rglob("*.py")) # recursively search the directory
+
+
+
+
 
 @click.command()
 @click.argument("filepath")
-
 def check(filepath):
-    """Run all Pylon validation rules on the specified Python file and print findings."""
+    """Run all Pylon validation rules on the specified Python file or directory and print findings."""
+    files = find_python_files(filepath)
+    click.echo(f"Validating {len(files)} file(s)...")
+
     all_findings = []
-    for rule in ALL_RULES:
-        all_findings.extend(rule(filepath))
+    for file in files:
+        for rule in ALL_RULES:
+            # need source file as well as the finding
+            findings = rule(str(file))
+            for finding in findings:
+                all_findings.append((file, finding)) # returning a zipped tuple for later report
+            
 
     if not all_findings: # if all_findings == []
         click.echo("No issues found! :)")
         return
 
-    for finding in sorted(all_findings, key=lambda f: f.line): # sorts findings by line number regardless of which rule found them
-        click.echo(f"{filepath}:{finding.line}: [{finding.rule_id}] {finding.message}")
+    for file, finding in sorted(all_findings, key=lambda pair: pair[1].line): # sorts by file by line number, with (file, _) used
+        click.echo(f"{file}:{finding.line}: [{finding.rule_id}] {finding.message}")
 
 if __name__ == "__main__":
     check()
